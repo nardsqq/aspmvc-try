a9f27dcf52f2b65f5f220345839224f3a03325b1
nardsqq <nardparagas@gmail.com>
2017-11-19 13:18:02 +0800

    Replaced default Bootstrap with Bootswatch Superhero

--- FirstASPProject/App_Start/BundleConfig.cs
+++ FirstASPProject/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace FirstASPProject
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-superhero.css",
                       "~/Content/site.css"));
         }
     }
---
+++ FirstASPProject/Controllers/SeriesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstASPProject.Models;
+
+namespace FirstASPProject.Controllers
+{
+    public class SeriesController : Controller
+    {
+        // GET: Series
+        public ActionResult Random()
+        {
+            var series = new Series() { Name = "Silicon Valley" };
+
+            return View(series);
+        }
+    }
+}
